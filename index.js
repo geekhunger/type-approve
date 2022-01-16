b2143df22609d76127daabc78b6e18a97cab983e
geekhunger <jack0088@me.com>
2022-01-16 15:47:49 +0000

    fixed bugs and improved across the board (typecheck handlers don't return arrays anymore, NodeJS14 dependency is lifted, improved error handling, and more)

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// NodeJS 14 or newer (because of optional-chaining operators)
-
-const {EOL} = require("os")
 let public = module.exports = {}
 let TYPE = Object.create(null)
 
 
+
+// private shortcuts (also used publically, see definitions at the bottom of this file!)
 const nil  = value => value === undefined || value === null || value === NaN
 const bool = value => (typeof value === "boolean" || typeof value === "string") && /^(true|false)$/i.test(value.toString().trim())
 const str  = value => typeof value === "string" && value.length > 0
@@ -34,9 +33,9 @@ const whois = function(typename) {
             }
         })
     switch(findings.length) {
-        case 0: return undefined
+        case 0: public.assert(false, `Missing typecheck handler for type '${typename}'!`)
         case 1: return findings[0]
-        default: this.assert(false, `Found too many validators (${findings.length}) of same type:${EOL}${JSON.stringify(findings, null, 4)}`)
+        default: public.assert(false, `Too many typecheck handlers (${findings.length}) of same type: ${JSON.stringify(findings, null, 4)}`)
     }
 }
 
@@ -51,9 +50,10 @@ public.type = function(singular, /*optional*/plural, handler) {
         return TYPE
     }
     if(nil(plural) && nil(handler)) {
-        return whois(singular)?.handler
+        try {return whois(singular).handler}
+        catch(_) {return undefined}
     }
-    this.assert(str(singular) && fn(handler), "Malformed values in typecheck call!")
+    public.assert(str(singular) && fn(handler), "Malformed typecheck call!")
     const name = `${singular}|${plural ?? singular + "s"}` // funny coincidence: the name is actually a valid RegExp expression
     TYPE[name] = handler
     return handler
@@ -62,121 +62,49 @@ public.type = function(singular, /*optional*/plural, handler) {
 
 
 public.check = function(...group) {
-
-    
-    let test = []
-
+    let result = []
     for(const set of group) {
         if(bool(set)) {
-            test.push(set)
+            result.push(set)
         } else if(obj(set)) {
-            for(const [typename, value] of Object.entries(set)) {
-                const check = whois(typename)
-
+            let test = []
+            for(const [typename, input] of Object.entries(set)) {
+                const validate = whois(typename)
+                switch(validate.name) {
+                    case validate.singular: {
+                        test.push(validate.handler(input))
+                        break
+                    }
+                    case validate.plural: {
+                        public.assert(arr(input), `Malformed value for '${validate.name}' typecheck!`)
+                        for(const value of input) test.push(validate.handler(value))
+                    }
+                }
             }
+            result.push(test.every(Boolean))
+        } else {
+            public.assert(false, `Unexpected value in typecheck call: ${JSON.stringify(set)}`)
         }
-        this.assert(false, "Unexpected value!")
     }
-
-
-    if(VARTYPE.boolean(map)[0]) {
-        return map
-    }
-    share.fn.assert(
-        VARTYPE.array(map)[0] || (VARTYPE.object(map)[0] && Object.keys(map).length === 1),
-        "Could not check types! Ruleset must be an object or an array of objects. Each object must have only one entry."
-    )
-    const validate = rule => {
-        let [type, value] = Object.entries(rule)[0] // unpack type name and value from rulecheck
-        const [negate, name] = type.match(/^(\!)*(.+)/).slice(1)
-        share.fn.assert(
-            typeof VARTYPE[name] === "function",
-            `Validation function for type '${name}' is not defined yet!`,
-            0
-        )
-        let [test, msg] = VARTYPE[name](value) // validate rule
-        if(negate === "!") test = !test // negate test result if type name is prefixed with exclamation mark
-        return test
-    }
-    if(VARTYPE.object(map)[0]) {
-        return validate(map)
-    }
-    if(map.every(value => VARTYPE.boolean(value)[0])) {
-        return map.every(bool => bool === true)
-    }
-    if(map.every(value => VARTYPE.object(value)[0])) {
-        return map.every(rule => validate(rule) === true)
-    }
-    const checks = []
-    for(const rule of map) {
-        checks.push(share.fn.typecheck(rule)) // dive deeper into nested ruleset
-    }
-    return checks.some(test => test === true)
+    return result.some(Boolean)
 }
 
 
 
-public.type("nil", value => [
-    value === undefined || value === null || value === NaN,
-    `Value '${value}' is not a valid 'nil'! Must be one of the falsy values: undefined, null or NaN.`
-])
-
-public.type("boolean", value => [
-    (typeof value === "boolean" || typeof value === "string") && /^(true|false)$/i.test(value.toString().trim()),
-    `Value '${value}' is not a valid 'boolean'! Must be true or false.`
-])
-
-public.type("number", value => [
-    /^\-?\d*\d\.?\d*$/.test(value) || Number(value) === 0,
-    `Value '${value}' is not a valid 'number'! Must be an integer or float.`
-])
-
-public.type("integer", value => [
-    /^\-?\d+$/.test(value) || Number(value) === 0,
-    `Value '${value}' is not a valid 'integer'! Must be a number without decimals.`
-])
-
-public.type("float", value => [
-    /^\-?\d+\d\.\d{2,}$/.test(value) || Number(value) === 0,
-    `Value '${value}' is not a valid 'float'! Must be a number with two decimals.`
-])
-
-public.type("string", value => [
-    typeof value === "string",
-    `Value '${value}' is not a valid 'string'!`
-])
-
-public.type("function", value => [
-    typeof value === "function",
-    `Value '${value}' is not a valid 'function'!`
-])
-
-public.type("promise", value => [
-    !Array.isArray(value) && (typeof value === "object" || typeof value === "function") && typeof value.then === "function",
-    `Value '${value}' is not a valid 'promise'! Must be an object (or a function) with a chainable 'then' property.`
-])
-
-public.type("object", value => [
-    typeof value === "object" && !Array.isArray(value) && value !== null,
-    `Value '${value}' is not a valid 'object'! Must NOT be an array or a null object!`
-])
-
-public.type("array", value => [
-    Array.isArray(value),
-    `Value '${value}' is not a valid 'array'!`
-])
-
-public.type("buffer", value => [
-    Buffer.isBuffer(value),
-    `Value '${value}' is not a valid 'buffer'!`
-])
-
-public.type("email", address => {
+public.type("nil",      nil)
+public.type("boolean",  bool)
+public.type("function", fn)
+public.type("object",   obj)
+public.type("array",    arr)
+public.type("number",   value   => /^\-?\d*\d\.?\d*$/.test(value) || Number(value) === 0)
+public.type("integer",  value   => /^\-?\d+$/.test(value) || Number(value) === 0)
+public.type("float",    value   => /^\-?\d+\d\.\d{2,}$/.test(value) || Number(value) === 0)
+public.type("string",   value   => typeof value === "string")
+public.type("promise",  value   => !Array.isArray(value) && (typeof value === "object" || typeof value === "function") && typeof value.then === "function")
+public.type("buffer",   value   => Buffer.isBuffer(value))
+public.type("email",    address => {
     const alphanumeric_set = "a-z0-9"
     const specialchars_set = "!#$%&'*+/=?^_`{|}~-"
     const validation_rule  = new RegExp(`^[${alphanumeric_set}${specialchars_set}]+(?:\.[${alphanumeric_set}${specialchars_set}]+)*@(?:[${alphanumeric_set}](?:[${alphanumeric_set}-]*[${alphanumeric_set}])?\.)+[${alphanumeric_set}](?:[${alphanumeric_set}-]*[${alphanumeric_set}])?$`, "gi") // regex found at https://regexr.com/2rhq7
-    return [
-        typeof address === "string" && address.match(validation_rule) !== null,
-        `Value '${address}' is not a valid 'email'! Must be a string that complies with general guidelines for naming email addresses.`
-    ]
+    return typeof address === "string" && address.match(validation_rule) !== null
 })
